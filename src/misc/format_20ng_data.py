d409598db35e3e3f36c0c5b5ef20b55a2ee4c17c
dallascard <dcard@andrew.cmu.edu>
2015-11-26 19:14:02 -0500

    updated export of 20ng data

--- src/misc/format_20ng_data.py
+++ src/misc/format_20ng_data.py
@@ -10,10 +10,9 @@ sys.path.append(os.getcwd())
 import file_handling as fh
 
 
-
 def main():
 
-    usage = "%prog"
+    usage = "%prog [category1 category2 ...]"
     parser = OptionParser(usage=usage)
     #parser.add_option('--keyword', dest='key', default=None,
     #                  help='Keyword argument: default=%default')
@@ -28,13 +27,14 @@ def main():
 
 
     (options, args) = parser.parse_args()
+    categories = args[:]
     remove_headers = options.remove_headers
     remove_footers = options.remove_footers
     remove_quotes = options.remove_quotes
-    export_20ng(remove_headers, remove_footers, remove_quotes)
+    export_20ng(remove_headers, remove_footers, remove_quotes, categories)
 
 
-def export_20ng(remove_headers=False, remove_footers=False, remove_quotes=False):
+def export_20ng(remove_headers=False, remove_footers=False, remove_quotes=False, categories=None):
     output_dir = os.path.join('..', 'datasets', '20ng', 'data')
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -47,15 +47,19 @@ def export_20ng(remove_headers=False, remove_footers=False, remove_quotes=False)
     if remove_quotes:
         remove.append('quotes')
 
-    ng_train = fetch_20newsgroups(subset='train', remove=remove)
+    print categories
+
+    ng_train = fetch_20newsgroups(subset='train', remove=remove, categories=categories)
     keys = ['train' + str(i) for i in range(len(ng_train.data))]
+    print len(keys)
     train_text = dict(zip(keys, ng_train.data))
     fh.write_to_json(train_text, os.path.join(output_dir, 'train.json'))
 
     train_labels = pd.DataFrame(ng_train.target, columns=['target'], index=keys)
     train_labels.to_csv(os.path.join(output_dir, 'train.csv'))
+    print train_labels.shape
 
-    ng_test = fetch_20newsgroups(subset='test', remove=remove)
+    ng_test = fetch_20newsgroups(subset='test', remove=remove, categories=categories)
     keys = ['test' + str(i) for i in range(len(ng_test.data))]
     test_text = dict(zip(keys, ng_train.data))
     fh.write_to_json(test_text, os.path.join(output_dir, 'test.json'))
