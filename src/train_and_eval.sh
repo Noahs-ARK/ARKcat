a73642ce2bd788417f8b802481d98683b82f7fb2
Jesse Dodge <dodgejesse@gmail.com>
2016-03-24 19:02:33 -0700

    simplifying running script

--- src/train_and_eval.sh
+++ src/train_and_eval.sh
@@ -1,9 +1,15 @@
+DATASET=trees_binary
+
+rm -r ../output
+python run.py ../../data/$DATASET/train.json ../../data/$DATASET/train.csv ../../data/$DATASET/dev.json ../../data/$DATASET/dev.csv ../output
+python eval.py ../output/saved_models/ ../../data/$DATASET/ ../output/
+
 #python run.py ../example_data/train.json ../example_data/train.csv ../example_data/dev.json ../example_data/dev.csv ../output
 
 #python run.py ../../data/atheistchristian/train.json ../../data/atheistchristian/train.csv ../../data/atheistchristian/dev.json ../../data/atheistchristian/dev.csv ../output
 
 #python run.py ../../data/trees_binary/train2.json ../../data/trees_binary/train2.csv ../../data/trees_binary/dev_train.json ../../data/trees_binary/dev_train.csv ../output
-python run.py ../../data/trees_binary/train.json ../../data/trees_binary/train.csv ../../data/trees_binary/dev.json ../../data/trees_binary/dev.csv ../output
+
 
 #python run.py ../../data/convote/train.json ../../data/convote/train.csv ../../data/convote/dev.json ../../data/convote/dev.csv ../output
 
