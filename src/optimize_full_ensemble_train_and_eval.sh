c2ab53e4560eadf1dab16581089598954bf113d1
Jesse Dodge <jessed@cab.ark.cs.cmu.edu>
2016-06-09 18:44:35 -0400

    removed dead code relating to ensembling and feature computation.

--- src/optimize_full_ensemble_train_and_eval.sh
+++
@@ -1,5 +0,0 @@
-DATASET=convote
-
-rm -r ../output
-python optimize_full_ensemble.py ../../data/$DATASET/train.json ../../data/$DATASET/train.csv ../../data/$DATASET/dev.json ../../data/$DATASET/dev.csv ../output
-python optimize_full_ensemble_eval.py ../output/saved_models/ ../../data/$DATASET/ ../output/
--- src/run_ensemble_eval.sh
+++
@@ -1,1 +0,0 @@
-python ensemble_of_trained_models.py /home/jesse/projects/ARKcat/output/saved_models/ ../../data/trees_binary/ ../output/
