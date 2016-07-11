85c27c07ae9a64cebaa6dbce0a622c6c2c50f260
Jesse Dodge <jessed@cab.ark.cs.cmu.edu>
2016-07-11 18:47:20 -0400

    passed model_dir to the cnn, so it can save temp models.

--- src/classify_test.py
+++ src/classify_test.py
@@ -58,16 +58,13 @@ def main():
 
 
 def classify(train_data_filename, train_label_filename, dev_data_filename, dev_label_filename, 
-             train_feature_dir, dev_feature_dir, feats_and_params, verbose=1, folds=-1):
-    m_and_d = Data_and_Model_Manager(feats_and_params)
+             train_feature_dir, dev_feature_dir, model_dir, feats_and_params, verbose=1, folds=-1):
+    m_and_d = Data_and_Model_Manager(feats_and_params, model_dir)
     m_and_d.load_train_and_dev_data(train_data_filename, train_label_filename, 
                                     train_feature_dir, dev_data_filename, dev_label_filename,
                                     dev_feature_dir, verbose)
     acc = m_and_d.k_fold_cv(folds)
-    
-#    train_acc = m_and_d.train_models()
-#    dev_acc = m_and_d.predict_acc_from_file(dev_data_filename, dev_label_filename)
-    
+        
     print('train acc: ' + str(acc['train_acc']))
     print('dev acc: ' + str(acc['dev_acc']))
     return {'loss': -acc['dev_acc'], 'status': STATUS_OK, 'model': m_and_d}
