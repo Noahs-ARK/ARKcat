ab69a51c0513239bd74c7caada708ef5c1ca0eba
EC2 Default User <dodgejesse@gmail.com>
2017-04-26 23:53:04 +0000

    now can request many spot instances

--- aws/request_many_spot.sh
+++ aws/request_many_spot.sh
@@ -1,1 +1,4 @@
-bash request_spot.sh dpp 0
+
+for i in `seq 1 10`; do
+    bash request_spot.sh dpp $i &
+done
--- aws/request_spot.sh
+++ aws/request_spot.sh
@@ -7,19 +7,28 @@ WAIT_SECONDS=1
 while true; do 
     SPOT_INST_ID=`aws ec2 describe-spot-instance-requests --spot-instance-request-ids $SPOT_REQUEST_ID | grep InstanceId | awk '{print $2}' | sed s/,// | sed s/\"// | sed s/\"//`
     if [ ! -z "$SPOT_INST_ID" ]; then
+	echo "successfully got spot instance id: $SPOT_INST_ID"
 	break
     else
 	echo "waiting $WAIT_SECONDS second(s) to check if spot request has been filled"
 	sleep $WAIT_SECONDS
     fi
 done
-echo "successfully got spot instance id: $SPOT_INST_ID"
+
 
 ###
 # to get the ip address:
-echo "about to get ip address..."
-SPOT_IP=`aws ec2 describe-instances --instance-ids $SPOT_INST_ID | grep PublicIpAddress | awk '{print $2}' | sed s/,// | sed s/\"// | sed s/\"// | sed 's/\./-/g'` 
-echo "successfully got ip address: $SPOT_IP"
+while true; do
+    SPOT_IP=`aws ec2 describe-instances --instance-ids $SPOT_INST_ID | grep PublicIpAddress | awk '{print $2}' | sed s/,// | sed s/\"// | sed s/\"// | sed 's/\./-/g'` 
+    if [ ! -z "$SPOT_IP" ]; then
+	echo "successfully got ip address: $SPOT_IP"
+	break
+    else
+	echo "waiting $WAIT_SECONDS second(s) to get the IP address"
+	sleep $WAIT_SECONDS
+    fi
+done
+
 
 while true; do
     STATE=`aws ec2 describe-instances --instance-ids $SPOT_INST_ID | grep \"Name\" | awk '{print $2}' | sed s/\"//g`
@@ -43,7 +52,6 @@ done
 # to copy the .pem file over:
 echo "about to copy .pem file over..."
 scp -i "/home/ec2-user/projects/ARKcat/aws/jesse-key-pair-uswest2.pem" -oStrictHostKeyChecking=no -r /home/ec2-user/projects/ARKcat/aws/jesse-key-pair-uswest2.pem ec2-user@ec2-${SPOT_IP}.us-west-2.compute.amazonaws.com:/home/ec2-user/
-scp -i "/home/ec2-user/projects/ARKcat/aws/jesse-key-pair-uswest2.pem" -oStrictHostKeyChecking=no -r /home/ec2-user/projects/ARKcat/src/train_and_eval_spot.sh ec2-user@ec2-${SPOT_IP}.us-west-2.compute.amazonaws.com:/home/ec2-user/projects/ARKcat/src/train_and_eval_spot.sh
 echo "copied!"
 
 
@@ -54,7 +62,7 @@ CUR_IP=`curl -s http://169.254.169.254/latest/meta-data/public-ipv4`
 
 ###
 # train models and move 
-ssh -i "/home/ec2-user/projects/ARKcat/aws/jesse-key-pair-uswest2.pem" -oStrictHostKeyChecking=no ec2-user@ec2-${SPOT_IP}.us-west-2.compute.amazonaws.com "source activate arkcat; cd /home/ec2-user/projects/ARKcat/src; bash train_and_eval_spot.sh ${1} ${2} $CUR_IP $SPOT_INST_ID"
+ssh -i "/home/ec2-user/projects/ARKcat/aws/jesse-key-pair-uswest2.pem" -oStrictHostKeyChecking=no ec2-user@ec2-${SPOT_IP}.us-west-2.compute.amazonaws.com "source activate arkcat; cd /home/ec2-user/projects/hyperopt; git checkout hyperopt/dpp.py; git pull; cd /home/ec2-user/projects/ARKcat/src; git checkout train_and_eval_pinot.sh; git pull; bash train_and_eval_spot.sh ${1} ${2} $CUR_IP $SPOT_INST_ID"
 
 
 ###
