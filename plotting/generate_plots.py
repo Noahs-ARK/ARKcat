929097e34f7d9c83f5610207caae560509f46f90
Jesse Dodge <jessedd@pinot.cs.washington.edu>
2017-06-02 15:06:36 -0700

    the current plotting. might not work to make scatters other than the panel of three

--- plotting/generate_plots.py
+++ plotting/generate_plots.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 import sample_histograms
 import scatter_with_error_bars
+import os
 
 def extract_single_example(example):
     example[4] = example[4].replace('7e-05', '0.00007')
@@ -59,7 +60,7 @@ def compute_avg_best_so_far(examples):
     avg_best = []
     avg_best_ci = []
     for i in range(len(best_so_far)):
-        avg_best_ci.append(1.96*np.std(np.asarray(best_so_far[i]))/np.sqrt(len(examples)))
+        avg_best_ci.append(2.575*np.std(np.asarray(best_so_far[i]))/np.sqrt(len(examples)))
         avg_best.append(np.average(np.asarray(best_so_far[i])))
 
     if False:
@@ -167,6 +168,54 @@ def get_avg_acc_by_val(examples):
     return avg_accs
                 
     
+def frac_vals_in_each_set(examples):
+    #hparam to fraction of time it's covered in one set
+    hparam_to_num_values = {}
+    hparam_to_num_poss_vals = {}
+    for set_of_k in examples:
+        seen_in_cur_set = {}
+        for example in set_of_k:
+            for hp in example[4]:
+
+                # to add to seen_in_cur_set:
+                if hp not in seen_in_cur_set:
+                    seen_in_cur_set[hp] = {}
+                v = example[4][hp]
+
+                if v == 1.0:
+                    continue
+
+                if v not in seen_in_cur_set[hp]:
+                    seen_in_cur_set[hp][v] = 0
+                seen_in_cur_set[hp][v] += 1
+
+                # to add hp to hparam_to_num_poss_vals:
+                if hp not in hparam_to_num_poss_vals:
+                    hparam_to_num_poss_vals[hp] = set()
+                hparam_to_num_poss_vals[hp].add(v)
+                    
+                
+        for hp in seen_in_cur_set:
+            if hp not in hparam_to_num_values:
+                hparam_to_num_values[hp] = []
+            hparam_to_num_values[hp].append(len(seen_in_cur_set[hp]))
+    # avg, std, med, max_seen, max, hp
+    for hp in hparam_to_num_values:
+        if max(hparam_to_num_values[hp]) != 1:
+            to_print = ' & {} & {} & {} & {} & {} & {} & {} \\\\'.format(
+                round(np.average(np.asarray(hparam_to_num_values[hp])),3),
+                round(np.std(np.asarray(hparam_to_num_values[hp])),3),
+                np.median(np.asarray(hparam_to_num_values[hp])),
+                max(hparam_to_num_values[hp]),
+                min(hparam_to_num_values[hp]),
+                len(hparam_to_num_poss_vals[hp]),
+                hp)
+            print to_print
+    
+    
+
+        
+
 
 def get_avg_and_std_dev(file_loc):
     with open(file_loc) as f:
@@ -174,6 +223,7 @@ def get_avg_and_std_dev(file_loc):
         examples = extract_examples(lines)
         avg_best, avg_best_ci = compute_avg_best_so_far(examples)
         total_counts, num_sets_each_val = frac_set_has_hp_value(examples)
+        frac_vals_in_each_set(examples)
         avg_acc_by_val = get_avg_acc_by_val(examples)
         
         
@@ -195,35 +245,178 @@ def make_into_dict(avg_best, avg_best_ci, prop_counts, num_sets_each_val, avg_ac
 def make_scatter_and_hist():
     #import pdb; pdb.set_trace() 
     all_info = {}
+    ordered_algos = ['dpp_rand',  'rand', 'tpe', 'dpp_ham', 'dpp' ]
+    names = {'dpp_rand':'Uniform-D', 'dpp_ham':'$k$-DPP-Hamm', 'dpp':'$k$-DPP-Cos',
+             'rand':'Uniform', 'tpe':'BO-TPE'}
     for space in ['reg_bad_lr', 'reg_half_bad_lr', 'reg']:
         all_info[space] = {}
-        for dist in ['dpp', 'dpp_ham', 'dpp_rand']:
-            things = get_avg_and_std_dev('/homes/gws/jessedd/projects/ARKcat/plotting/results/20_iter/stanford_sentiment_binary,mdl_tpe=cnn,srch_tpe={},spce={}.txt'.format(dist, space))
-            all_info[space][dist] = make_into_dict(*things)
+        for dist in ordered_algos:
+            file_name = '/homes/gws/jessedd/projects/ARKcat/plotting/results/20_iter/stanford_sentiment_binary,mdl_tpe=cnn,srch_tpe={},spce={}.txt'.format(dist, space)
+            
+            if os.path.isfile(file_name):
+                things = get_avg_and_std_dev(file_name)
+                all_info[space][dist] = make_into_dict(*things)
+                
         
     counter = 0
-    matplotlib.rcParams.update({'font.size': 4})
+    matplotlib.rcParams.update({'font.size': 6})
+    fig = plt.figure()
+    #lines = ['-','--',':','-.','_']
+    dash_list = [(5,2,20,2),(5,2,10,2),(5,2),(10,2),(3,2,2,2)] 
     for space in ['reg_bad_lr', 'reg_half_bad_lr', 'reg']:
-        counter = counter + 1
-        plt.subplot(2,3,counter)
-        scatter_with_error_bars.add_scatter(all_info[space]['dpp'], all_info[space]['dpp_ham'], all_info[space]['dpp_rand'], space)
 
-    for space in ['reg_bad_lr', 'reg_half_bad_lr', 'reg']:
         counter = counter + 1
-        plt.subplot(2,3,counter)
-        sample_histograms.add_hist(all_info[space]['dpp'], all_info[space]['dpp_ham'], 'num_sets_each_val')
-
-    plt.savefig('plot_drafts/DEBUG_TRYING_ALL_THREE.pdf')
+        cur_ax = fig.add_subplot(1,3,counter, adjustable='box', aspect=100)
+        data_for_scatter = {}
+        dash_counter = 0
+        for dist in ordered_algos:
+            if dist in all_info[space]:
+                data_for_scatter[dist] = {}
+                data_for_scatter[dist]['avg_best'] = all_info[space][dist]['avg_best']
+                data_for_scatter[dist]['dash'] = dash_list[dash_counter]
+                dash_counter += 1        
+        scatter_with_error_bars.add_scatter(data_for_scatter, ordered_algos, names, space, cur_ax)
     
-
+        
+    plt.tight_layout()
+    plt.savefig('plot_drafts/DEBUG_THREE_MEDIAN.pdf',bbox_inches='tight')
+    
+def make_arch_scatter():
+    all_info = {}
+    for dist in ['dpp', 'dpp_ham', 'dpp_rand']:
+        things = get_avg_and_std_dev('/homes/gws/jessedd/projects/ARKcat/plotting/results/20_iter/stanford_sentiment_binary,mdl_tpe=cnn,srch_tpe={},spce=arch.txt'.format(dist))
+        all_info[dist] = make_into_dict(*things)
+    fig = plt.figure()
+    cur_ax = fig.add_subplot(1,1,1)
+    scatter_with_error_bars.add_scatter(all_info['dpp'], all_info['dpp_ham'], all_info['dpp_rand'], 'arch', cur_ax)
+    plt.tight_layout()
+    plt.savefig('plot_drafts/DEBUG_ARCH.pdf',bbox_inches='tight')
             
     
-make_scatter_and_hist()
+def make_linear_scatter():
+    all_info = {}
+    for dist in ['dpp', 'dpp_rand']:
+        things = get_avg_and_std_dev('/homes/gws/jessedd/projects/ARKcat/plotting/results/50_iter/stanford_sentiment_binary,mdl_tpe=linear,srch_tpe={}.txt'.format(dist))
+        all_info[dist] = make_into_dict(*things)
+
+
+    #print all_info['dpp']['avg_best'][-1]
+    #print all_info['dpp']['ci'][-1]
+    #print all_info['dpp_rand']['avg_best'][-1]
+    #print all_info['dpp_rand']['ci'][-1]
+    #sys.exit(0)
+
+
+    fig = plt.figure()
+    cur_ax = fig.add_subplot(1,2,1)
+    scatter_with_error_bars.add_scatter(all_info['dpp'], None, all_info['dpp_rand'], 'linear', cur_ax)
+    
+
+
+    all_info['dpp']['avg_best'] = all_info['dpp']['avg_best'][25:]
+    all_info['dpp_rand']['avg_best'] = all_info['dpp_rand']['avg_best'][25:]
+    
+
+    cur_ax = fig.add_subplot(1,2,2)
+    scatter_with_error_bars.add_scatter(all_info['dpp'], None, all_info['dpp_rand'], 'linear', cur_ax, .78, 0.795, 24)
+    
+    plt.tight_layout()
+    plt.savefig('plot_drafts/DEBUG_LINEAR.pdf',bbox_inches='tight')
+
+
+def make_empirical_hists():
+    all_info = {}
+    for space in ['reg_bad_lr', 'reg_half_bad_lr', 'reg']:
+        things = get_avg_and_std_dev('/homes/gws/jessedd/projects/ARKcat/plotting/results/20_iter/stanford_sentiment_binary,mdl_tpe=cnn,srch_tpe=dpp_ham,spce={}.txt'.format(space))
+        all_info[space] = make_into_dict(*things)
+    new_all_info = {}
     
     
+    new_all_info['all'] = {}
+    new_all_info['all']['prop_counts'] = {}
+
+    #import pdb; pdb.set_trace() 
+    for space in ['reg_bad_lr', 'reg_half_bad_lr']:
+        for hparam in all_info[space]['prop_counts']:
+            if hparam not in new_all_info['all']['prop_counts']:
+                new_all_info['all']['prop_counts'][hparam] = {}
+            new_all_info['all']['prop_counts'][hparam]['vals'] = all_info[space]['prop_counts'][hparam]['vals']
+            if 'prop' not in new_all_info['all']['prop_counts'][hparam]:
+                new_all_info['all']['prop_counts'][hparam]['prop'] = [i*all_info[space]['num_samples']*all_info[space]['num_iters'] for i in all_info[space]['prop_counts'][hparam]['prop']]
+            else:
+                for i in range(len(new_all_info['all']['prop_counts'][hparam]['prop'])):
+                    new_all_info['all']['prop_counts'][hparam]['prop'][i] += all_info[space]['prop_counts'][hparam]['prop'][i]*all_info[space]['num_samples']*all_info[space]['num_iters']
+
+
+    total_samples = 0
+    for space in ['reg_bad_lr', 'reg_half_bad_lr']:
+        total_samples += all_info[space]['num_samples']*all_info[space]['num_iters']
+    for hparam in new_all_info['all']['prop_counts']:
+        new_all_info['all']['prop_counts'][hparam]['prop'] = [i/total_samples for i in new_all_info['all']['prop_counts'][hparam]['prop']]
+
+    
+
+    print new_all_info['all']['prop_counts']['reg_strength']['prop']
+    del new_all_info['all']['prop_counts']['reg_strength']['prop'][-1]
+    print new_all_info['all']['prop_counts']['reg_strength']['prop']
+
+        
+
+    
+
+    dpp_info = sample_histograms.get_data()
+    
+    matplotlib.rcParams.update({'font.size': 7})
+    fig = plt.figure()
+    counter = 0
+    for hparam in new_all_info['all']['prop_counts']:
+        counter += 1
+        cur_ax = fig.add_subplot(2,2,counter)
+        if hparam == 'learning_rate':
+            handles = sample_histograms.add_hist(dpp_info, new_all_info['all'], 'prop_counts', hparam, 'learning rate')
+        elif hparam == 'reg_strength':
+            handles = sample_histograms.add_hist(dpp_info, new_all_info['all'], 'prop_counts', hparam, 'regularization strength')
+        elif hparam == 'dropout':
+            handles = sample_histograms.add_hist(dpp_info, new_all_info['all'], 'prop_counts', hparam, 'dropout')
+        elif hparam == 'regularizer':
+            handles = sample_histograms.add_hist(dpp_info, new_all_info['all'], 'prop_counts', hparam, 'regularization')
+    
+    fig.legend(handles, ('k-DPP-Cos','k-DPP-Hamm'), loc='lower right')
+    
+    plt.tight_layout()
+    plt.savefig('plot_drafts/DEBUG_FOUR_HISTS.pdf',bbox_inches='tight')
+    
+
+    #for hparam in all_info
+    #new_all_info['all']['prop_counts'] = new_all_info['all']['prop_counts']*1.0/(all_info['reg_bad_lr']['num_samples'] + all_info['reg_half_bad_lr']['num_samples'])
+    
+    #print new_all_info['all']['prop_counts']
+    
+        
+    
+def print_best_acc():
+    for space in ['reg_bad_lr', 'reg_half_bad_lr', 'reg', 'arch']:
+        for dist in ['dpp_rand', 'dpp_ham', 'dpp']:
+
+            avg_best, avg_best_ci, prop_counts, num_sets_each_val, avg_acc_by_val, num_samples, num_iters = get_avg_and_std_dev('/homes/gws/jessedd/projects/ARKcat/plotting/results/20_iter/stanford_sentiment_binary,mdl_tpe=cnn,srch_tpe={},spce={}.txt'.format(dist, space))
+            low_ci = round(100*avg_best[len(avg_best)-1]-avg_best_ci[len(avg_best_ci)-1],3)
+            high_ci = round(100*avg_best[len(avg_best)-1]+avg_best_ci[len(avg_best_ci)-1],3)
+            print '{}, {}: {} & \small ({},{}), {}'.format(space, dist, round(100*avg_best[len(avg_best)-1],3), low_ci, high_ci, avg_best_ci[len(avg_best_ci)-1])
+    
 
 
 
+#make_scatter_and_hist()
+#make_arch_scatter()
+#make_linear_scatter()
+#make_empirical_hists()
+#print_best_acc()
+
+
+things = get_avg_and_std_dev('/homes/gws/jessedd/projects/ARKcat/plotting/results/20_iter/stanford_sentiment_binary,mdl_tpe=cnn,srch_tpe=dpp_rand,spce=reg_half_bad_lr.txt')
+things = get_avg_and_std_dev('/homes/gws/jessedd/projects/ARKcat/plotting/results/20_iter/stanford_sentiment_binary,mdl_tpe=cnn,srch_tpe=dpp_ham,spce=reg_half_bad_lr.txt')
+things = get_avg_and_std_dev('/homes/gws/jessedd/projects/ARKcat/plotting/results/20_iter/stanford_sentiment_binary,mdl_tpe=cnn,srch_tpe=dpp,spce=reg_half_bad_lr.txt')
+#all_info[space] = make_into_dict(*things)
 
 
 
@@ -232,9 +425,9 @@ space = 'reg_half_bad_lr'
 model = 'cnn'
 dist = '_ham'
 
-dpp_avg_best, dpp_avg_best_ci, dpp_prop_counts, dpp_num_sets_each_val, dpp_avg_acc_by_val, dpp_num_samples, dpp_num_iters = get_avg_and_std_dev('/homes/gws/jessedd/projects/ARKcat/plotting/results/{}_iter/stanford_sentiment_binary,mdl_tpe={},srch_tpe=dpp{},spce={}.txt'.format(iters, model, dist, space))
+#dpp_avg_best, dpp_avg_best_ci, dpp_prop_counts, dpp_num_sets_each_val, dpp_avg_acc_by_val, dpp_num_samples, dpp_num_iters = get_avg_and_std_dev('/homes/gws/jessedd/projects/ARKcat/plotting/results/{}_iter/stanford_sentiment_binary,mdl_tpe={},srch_tpe=dpp{},spce={}.txt'.format(iters, model, dist, space))
 
-rand_avg_best, rand_avg_best_ci, rand_prop_counts, rand_num_sets_each_val, rand_avg_acc_by_val, rand_num_samples, rand_num_iters = get_avg_and_std_dev('/homes/gws/jessedd/projects/ARKcat/plotting/results/{}_iter/stanford_sentiment_binary,mdl_tpe={},srch_tpe=dpp_rand,spce={}.txt'.format(iters, model, space))
+#rand_avg_best, rand_avg_best_ci, rand_prop_counts, rand_num_sets_each_val, rand_avg_acc_by_val, rand_num_samples, rand_num_iters = get_avg_and_std_dev('/homes/gws/jessedd/projects/ARKcat/plotting/results/{}_iter/stanford_sentiment_binary,mdl_tpe={},srch_tpe=dpp_rand,spce={}.txt'.format(iters, model, space))
 #rand_avg_best2, rand_avg_best_ci2, rand_prop_counts2, rand_num_sets_each_val2, rand_avg_acc_by_val2, rand_num_samples2, rand_num_iters2 = get_avg_and_std_dev('/homes/gws/jessedd/projects/ARKcat/plotting/results/{}_iter/stanford_sentiment_binary,mdl_tpe={},srch_tpe=dpp_rand,spce={}_SECOND_HUNDRED.txt'.format(iters, model, space))
 
 
--- plotting/scatter_with_error_bars.py
+++ plotting/scatter_with_error_bars.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 
 
 def make_scatter(dpp_y, dpp_err, rand_y, rand_err, dpp_num_samples, rand_num_samples, dpp_num_iters, space):
@@ -20,19 +21,61 @@ def make_scatter(dpp_y, dpp_err, rand_y, rand_err, dpp_num_samples, rand_num_sam
 
 
 
-def add_scatter(dpp_info, ham_info, rand_info, space):
-    x = range(1,len(dpp_info['avg_best'])+1)
-    dpp_x = [i-.15 for i in x]
-    ham_x = x
-    rand_x = [i+.15 for i in x]
-    
-    plt.ylim([.53,.83])
-    #plt.errorbar(dpp_x, dpp_info['avg_best'], yerr=dpp_info['ci'], fmt=',',elinewidth=.5,markersize=2)
-    #plt.errorbar(ham_x, ham_info['avg_best'], yerr=ham_info['ci'], fmt=',',elinewidth=.5, markersize=2)
-    #plt.errorbar(rand_x, rand_info['avg_best'], yerr=rand_info['ci'], fmt=',',elinewidth=.5, markersize=2)
-    plt.plot(rand_x, rand_info['avg_best'])
-    plt.plot(ham_x, ham_info['avg_best'])
+def add_scatter(data, order, names, space, cur_ax, ylim_lower=None, ylim_upper=None, last_few=None):
+    x = range(1,len(data['dpp']['avg_best'])+1)
+    if last_few is not None:
+        x = [xi + last_few for xi in x]
+    diff_x = {}
+    counter = 0
+    width = .3
+    for algo in order:
+        counter += 1
+        #diff_x[algo] = [(width*1.0/len(data)) * counter + x_i for x_i in x]
+        diff_x[algo] = x
+    #dpp_x = [i-.15 for i in x]
+    #ham_x = x
+    #rand_x = [i+.15 for i in x]
 
-    plt.plot(dpp_x, dpp_info['avg_best'])
-    plt.title("Mean best dev acc after K iterations on {}".format(space))
-    plt.legend(["Random", "DPP-Hamm", "DPP"],loc='lower right')
+    if space != 'arch':
+        cur_ax.set_ylim([.545,.83])
+    if ylim_lower is not None:
+        cur_ax.set_ylim([ylim_lower,ylim_upper])
+
+    #cur_ax.errorbar(rand_x, rand_info['avg_best'], yerr=rand_info['ci'], fmt=',',elinewidth=.5, markersize=2)
+    #cur_ax.errorbar(dpp_x, dpp_info['avg_best'], yerr=dpp_info['ci'], fmt=',',elinewidth=.5,markersize=2)
+    #cur_ax.errorbar(ham_x, ham_info['avg_best'], yerr=ham_info['ci'], fmt=',',elinewidth=.5, markersize=2)
+    for algo in order:
+        if algo in data:
+            cur_ax.plot(diff_x[algo], data[algo]['avg_best'], dashes=data[algo]['dash'], linewidth=1)
+    #cur_ax.plot(rand_x, rand_info['avg_best'], '-', linewidth=1)
+    #if ham_info is not None:
+    #    cur_ax.plot(ham_x, ham_info['avg_best'], '--', linewidth=1)
+    #cur_ax.plot(dpp_x, dpp_info['avg_best'],':', linewidth=1)
+
+    if space == 'reg_bad_lr':
+        cur_ax.set_title("Hard learning rate".format(space))
+    elif space == 'reg_half_bad_lr':
+        cur_ax.set_title("Medium learning rate".format(space))
+    elif space == 'reg':
+        cur_ax.set_title("Easy learning rate".format(space))
+    
+    cur_ax.legend([names[name] for name in order if name in data], loc='lower right')
+    #if ham_info is not None:
+    #    cur_ax.legend(["Uniform", "k-DPP-Hamm", "k-DPP-Cos"],loc='lower right')
+    #else:
+    #    cur_ax.legend(["Uniform", "k-DPP-Cos"],loc='lower right')
+
+    cur_ax.xaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
+
+
+    #cur_ax.locator_params(axis='x', nticks=11)
+
+
+    #num_vals = len(x)
+    #pos = np.arange(num_vals)
+    #ticks = []
+    #for i in range(10):
+    #    ticks.append(str(i*2 + 1))
+    #    ticks.append('')
+    #print ticks
+    #cur_ax.set_xticks(ticks)
