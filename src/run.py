0d637e0496bc9592510a19955f6848ec820f4690
EC2 Default User <dodgejesse@gmail.com>
2017-10-26 18:27:55 +0000

    trying mixed_dpp_rbf_narrow

--- src/run.py
+++ src/run.py
@@ -267,6 +267,10 @@ def main(args):
             trials.dpp_dist = "rbf_clip"
             algorithm = dpp.suggest
             trials.discretize_space = False
+        elif args['algorithm'] == 'mixed_dpp_rbf_narrow':
+            trials.dpp_dist = "rbf_narrow"
+            algorithm = dpp.suggest
+            trials.discretize_space = False
         elif args['algorithm'] == "dpp_random":
             algorithm = dpp_random.suggest
         else:
